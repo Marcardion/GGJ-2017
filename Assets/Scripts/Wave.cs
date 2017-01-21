599b69b62521e4e31521cbf08f08e538be60cd74
Qlin <emanuell.amorim.lima@gmail.com>
2017-01-21 08:49:55 -0400

    SoundWave v0.1
    Added an idea of how can be the soundwave.

---
+++ Assets/Scripts/Wave.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave : MonoBehaviour {
+
+	private GameObject player;
+	private Vector3 maxScale = new Vector3(4,4,1);
+
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = player.transform.position;
+
+		PulseWave();
+	}
+
+	void PulseWave() {
+		if((transform.localScale - maxScale).sqrMagnitude < 0.1){
+			transform.localScale = Vector3.one;
+		} else {
+			transform.localScale = Vector3.Lerp(transform.localScale, maxScale, Time.deltaTime);
+		}
+	}
+}
