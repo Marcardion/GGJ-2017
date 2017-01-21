90f3c225e25a7f4352a72da22b2925f3bcc08996
Luiz Marcelo <marcardion@gmail.com>
2017-01-21 01:19:48 -0300

    Criando o Repositorio

---
+++ Assets/Scripts/Enemy_AI.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;	
+using UnityEngine.AI;
+
+public class Enemy_AI : MonoBehaviour {
+
+	public Transform target;
+	public Vector3 start_pos;
+
+	NavMeshAgent agent;
+
+	public bool move = false;
+
+	// Use this for initialization
+	void Start () {
+
+		agent = GetComponent<NavMeshAgent> ();
+		agent.updateRotation = false;
+
+		start_pos = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 direction = target.position - transform.position;
+
+		Debug.DrawRay (transform.position, direction, Color.black);
+
+		if (move) {
+			agent.SetDestination (target.position);
+		} else {
+			agent.SetDestination (start_pos);
+		}
+
+	}
+
+	void  OnTriggerStay (Collider collider)
+	{
+		if (collider.CompareTag ("Player")) 
+		{
+			RaycastHit hit;
+
+			Vector3 direction = target.position - transform.position;
+		
+			if (Physics.Raycast (transform.position, direction, out hit)) 
+			{
+				
+				if (hit.collider.CompareTag ("Player")) 
+				{
+					move = true;
+				}
+				else 
+				{
+					move = false;
+				}
+
+			}
+
+		}
+	}
+
+	void OnTriggerExit (Collider collider)
+	{
+		if (collider.CompareTag ("Player")) 
+		{
+			move = false;
+		}
+	}
+}
---
+++ Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Movement : MonoBehaviour {
+
+
+	private Rigidbody my_rigidbody;
+
+	private Vector2 interp_axis;
+
+	public float move_speed;
+
+	// Use this for initialization
+	void Start () {
+
+		my_rigidbody = GetComponent<Rigidbody> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Move ();
+	
+	}
+
+	void Move()
+	{
+		InterpolateAxis ();
+
+		Vector3 move_vector = new Vector3(move_speed*interp_axis.x, my_rigidbody.velocity.y, move_speed*interp_axis.y);
+
+		my_rigidbody.velocity = move_vector;
+	}
+
+	public void PlaceMove(Vector3 position)
+	{
+		transform.position = new Vector3(transform.position.x + position.x,transform.position.y + position.y,transform.position.z + position.z);
+	}
+
+	void InterpolateAxis()
+	{
+		interp_axis = Vector2.Lerp (interp_axis, new Vector2 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical")), Time.fixedDeltaTime*3f);
+	}
+}
